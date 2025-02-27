8f79b2d392373f22f1b76e57cc3d528b8480bb83
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-02-26 22:42:03 -0300

    feat: temperature in a thermometer

---
+++ src/components/TemperaturePanel.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Thermometer from 'react-thermometer-component';
+
+import "./TemperaturePanel.css"
+
+const TemperaturePanel: React.FC = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const hideLabel = () => {
+    document.querySelector(".thermometer__percent-current")?.classList.add("hidden");
+    setIsHovered(false);
+  }
+
+  const showLabel = () => {
+    document.querySelector(".thermometer__percent-current")?.classList.remove("hidden");
+    setIsHovered(true);
+  }
+
+  return <div className="panel" id="temperature-panel">
+    <h2>Temperature</h2>
+    <div id="thermometer-container" onMouseEnter={showLabel} onMouseLeave={hideLabel}>
+      <Thermometer
+        theme="light"
+        value="25"
+        max="100"
+        steps="1"
+        format="°C"
+        size="normal"
+        height="300"
+      />
+    </div>
+  </div>
+}
+
+export default TemperaturePanel;
