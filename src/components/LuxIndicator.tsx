c07546ad9f19649ab4e4789f0a33d1aa3e7bb617
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-02-27 09:40:26 -0300

    feat: add api service to component

--- src/components/LuxIndicator.tsx
+++ src/components/LuxIndicator.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
+import apiServices from '../services/apiServices';
 
 const LuxIndicator: React.FC = () => {
+  const [lux, setLux] = useState(0);
+
+  useEffect(() => {
+
+    const interval = setInterval(async () => {
+      const data = await apiServices.getLux();
+      setLux(Number(data.lux.toFixed(0)));
+    }, 100);
+    // Clean up to avoid background executions
+    return () => clearInterval(interval);
+  }, []);
 
   return <Gauge
-    value={75}
+    value={lux}
     startAngle={180}
     endAngle={360}
     innerRadius="60%"
