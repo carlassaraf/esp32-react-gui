59b2342a4694c77fb7698cade320bbbc1c65f8e0
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-02-27 03:27:07 -0300

    feat: add api service to component

--- src/components/BuzzerControl.tsx
+++ src/components/BuzzerControl.tsx
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import { ToggleButton } from "@mui/material";
 import VolumeOffIcon from '@mui/icons-material/VolumeOff';
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import apiServices from "../services/apiServices";
 
 // Simple toggle button using volume icons
 
 const BuzzerControl: React.FC = () => {
   const [buzzState, setBuzzState] = useState(false);
 
-  const handleClick = () => {
+  const handleClick = async () => {
     setBuzzState(!buzzState);
+    const data = await apiServices.setBuzzerState(!buzzState);
+    if(data.status !== 200) { console.error("Failed to fetch"); }
   }
 
   return <ToggleButton value={!buzzState} onClick={handleClick}>
