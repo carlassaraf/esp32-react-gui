8df1109d75cfdd2656986bf2760816c449d77ef1
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-02-26 23:51:19 -0300

    feat: panel for button status

---
+++ src/components/GPIOButton.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+
+interface GPIOButtonProps {
+  name: string;
+  pin: number;
+}
+
+const GPIOButton: React.FC<GPIOButtonProps> = ({name, pin}) => {
+  const [state, changeState] = useState(false);
+  const icons: Record<string, React.ElementType> = {
+    izq: ArrowCircleLeftIcon,
+    enter: ArrowCircleUpIcon,
+    der: ArrowCircleRightIcon
+  };
+
+  const Icon = icons[name.toLowerCase()];
+
+  return <div className="btn-container">
+    {Icon && <Icon fontSize="large" />}
+    <h5>{name}</h5>
+    <h6>{`Pin ${pin}`}</h6>
+  </div>
+}
+
+export default GPIOButton;
---
+++ src/components/GPIOButtonPanel.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import GPIOButton from "./GPIOButton";
+import "./GPIOButtonPanel.css"
+
+interface GPIOButtonPanelProps {
+  names: Array<string>;
+  pins: Array<number>;
+}
+
+const GPIOButtonPanel: React.FC<GPIOButtonPanelProps> = ({names, pins}) => {
+
+  return <div className="panel" id="button-panel">
+    <h2>Buttons</h2>
+    {
+      names.map((name, i) => {
+        return <GPIOButton name={name} pin={pins[i]}></GPIOButton>
+      })
+    }
+  </div>
+}
+
+export default GPIOButtonPanel;
