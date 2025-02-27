96d967a3eac598ff24b471ea359e18fd2ad88cad
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-02-27 11:14:16 -0300

    feat: add api service to component

--- src/components/GPIOButton.tsx
+++ src/components/GPIOButton.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import apiServices from "../services/apiServices";
 
 interface GPIOButtonProps {
   name: string;
@@ -9,18 +10,27 @@ interface GPIOButtonProps {
 }
 
 const GPIOButton: React.FC<GPIOButtonProps> = ({name, pin}) => {
-  // For later
-  // const [state, changeState] = useState(false);
+  const [state, changeState] = useState<any>({});
   const icons: Record<string, React.ElementType> = {
-    izq: ArrowCircleLeftIcon,
+    left: ArrowCircleLeftIcon,
     enter: ArrowCircleUpIcon,
-    der: ArrowCircleRightIcon
+    right: ArrowCircleRightIcon
   };
+
+  useEffect(() => {
+
+    const interval = setInterval(async () => {    
+      const data = await apiServices.getButtons();
+      changeState(data.buttons);
+    }, 250);
+    // Clean up to avoid background executions
+    return () => clearInterval(interval);
+  }, []);
 
   const Icon = icons[name.toLowerCase()];
 
   return <div className="btn-container">
-    {Icon && <Icon fontSize="large" />}
+    {Icon && <Icon fontSize="large" sx={{fill: state[name.toLowerCase()]? "white" : "green"}}/>}
     <h5>{name}</h5>
     <h6>{`Pin ${pin}`}</h6>
   </div>
