2ec3d81c89e8fdcad8e4e14fe108cfc1ab538c11
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-02-27 09:36:56 -0300

    feat: add api service to component

--- src/components/TemperaturePanel.tsx
+++ src/components/TemperaturePanel.tsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Thermometer from 'react-thermometer-component';
 
 import "./TemperaturePanel.css"
+import apiServices from '../services/apiServices';
 
 const TemperaturePanel: React.FC = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [temperature, setTemperature] = useState(25);
 
   const hideLabel = () => {
     document.querySelector(".thermometer__percent-current")?.classList.add("hidden");
@@ -18,6 +20,13 @@ const TemperaturePanel: React.FC = () => {
 
   useEffect(() => {
     hideLabel();
+
+    const interval = setInterval(async () => {
+      const temp = await apiServices.getTemperature();
+      setTemperature(Number(temp.temperature.toFixed(1)));
+    }, 500);
+    // Clean up to avoid background executions
+    return () => clearInterval(interval);
   }, []);
 
   return <div className="panel" id="temperature-panel">
@@ -25,7 +34,7 @@ const TemperaturePanel: React.FC = () => {
     <div id="thermometer-container" onMouseEnter={showLabel} onMouseLeave={hideLabel}>
       <Thermometer
         theme="light"
-        value="25"
+        value={temperature}
         max="100"
         steps="1"
         format="°C"
